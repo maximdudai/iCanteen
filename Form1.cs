cd4f72bc75085be1c2e051f4735e86a69985fe7d
ana-fg <146133781+ana-fg@users.noreply.github.com>
2024-05-25 15:00:25 +0100

    form recreaction

--- Form1.cs
+++ Form1.cs
@@ -7,10 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using PSI_DA_PL_B;
-using PSI_DA_PL_B.models.Utilizador;
-using PSI_DA_PL_B.models.MenuCantina;
-
 
 namespace PSI_DA_PL_B
 {
@@ -19,18 +15,6 @@ namespace PSI_DA_PL_B
         public Form1()
         {
             InitializeComponent();
-
-            // exemplo codigo para guardar na base de dados.
-            using (var db = new Cantina())
-            {
-                var utilizador = new Utilizador { Nome = "Joao", Nif = 123321123 };
-                db.Utilizador.Add(utilizador);
-                db.SaveChanges();
-
-                var menu = new MenuCantina { data = DateTime.Now, quantidade = 10, preco_estudante = 2.5, preco_professor = 3.5 };
-                db.MenuCantina.Add(menu);
-                db.SaveChanges();
-            }
         }
     }
 }
