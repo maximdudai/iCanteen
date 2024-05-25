b514cb036fb923b2dfef456ac0600887830976f5
maximdudai <maxim.dudai01@gmail.com>
2024-05-25 23:22:27 +0100

    SDM-11 Authentication UI & Helpers
    - Login UI
    - Login: Username limited to 64 characters
    - Helpers: Method to validate data using RegEx
    - Helpers: Custom errors
    - User now has a function row that indicate if it's employee or client (student or professor)
    - Employee list UI

---
+++ helpers/Error.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSI_DA_PL_B.helpers
+{
+    public static class Error
+    {
+        public static void Warning(string message)
+        {
+            System.Windows.Forms.MessageBox.Show(message, "Warning", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning);
+        }
+        public static void Err(string message)
+        {
+            System.Windows.Forms.MessageBox.Show(message, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ views/Auth/Employees/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PSI_DA_PL_B.models.Utilizador;
+
+namespace PSI_DA_PL_B.views.Auth.Employees
+{
+    public partial class Employee : Form
+    {
+        private List<Funcionario> funcionarios;
+        public Employee()
+        {
+            InitializeComponent();
+            funcionarios = new List<Funcionario>();
+        }
+    }
+}
