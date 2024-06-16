45538e40121810fd1cf590bf19d67854c35ac426
maximdudai <maxim.dudai01@gmail.com>
2024-06-16 22:44:08 +0100

    Controller

--- controller/Manager.cs
+++ controller/Manager.cs
@@ -78,7 +78,7 @@ namespace PSI_DA_PL_B.controller
             ShowForm(ref registerForm, toggle, this);
         }
 
-        public void MainMenuUI(bool toggle = true, string username = "")
+        public void MainMenuUI(string username, bool toggle = true)
         {
             ShowForm(ref mainMenuForm, toggle, this, username);
         }
